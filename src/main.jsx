afb8d37569221ad37452096afaf22699e042ba4b
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-02 15:29:03 -0500

    mi boton con hola Mundo

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,5 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 const root = createRoot(document.getElementById("root"));
-root.render("Hola Mundo");
+// root.render("Hola Mundo");
+root.render(<button>Hola Mundo</button>);
