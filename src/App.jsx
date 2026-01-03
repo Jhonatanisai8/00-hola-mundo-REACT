c909256848d99cde6c12bb0839fe9dff03f22cbb
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-03 11:17:48 -0500

    inicializar una aplicación de React con estilos globales, el componente App y el estilo de la tarjeta de seguimiento de Twitter.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   return (
-    <>
+    <section className="App">
       <TwitterFollowCard userName="midudev" name="Midudev" />
       <TwitterFollowCard userName="mouredev" name="Mouredev" />
       <TwitterFollowCard userName="jhonatan" name="Jhonatan" />
-    </>
+    </section>
   );
 }
