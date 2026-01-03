cc858d970f4fe0905eb815b5b5be4d48e27ab800
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-03 11:13:30 -0500

    Agrega el componente TwitterFollowCard y muestra varias instancias en la aplicación.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import "./App.css";
+import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   return (
-    <article className="tw-followCard">
-      <header className="tw-followCard-header">
-        <img
-          className="tw-followCard-avatar"
-          src="https://unavatar.io/kikobeats"
-          alt="Kikobets"
-        />
-        <div className="tw-followCard-info">
-          <strong>Kikobeats</strong>
-          <span className="tw-followCard-info-username">@kikobeats</span>
-        </div>
-      </header>
-      <aside>
-        <button className="tw-followCard-button">Seguir</button>
-      </aside>
-    </article>
+    <>
+      <TwitterFollowCard userName="midudev" name="Midudev" />
+      <TwitterFollowCard userName="mouredev" name="Mouredev" />
+      <TwitterFollowCard userName="jhonatan" name="Jhonatan" />
+    </>
   );
 }
