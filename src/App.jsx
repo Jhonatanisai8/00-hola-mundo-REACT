64cf06574c0a56cb308da0d19ef8002e54d00fb1
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-01-07 22:17:41 -0500

    otro ejemplo de  useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 export function App() {
   // const addAt = (userName) => `@${userName}`;
   // const format = (userName) => `@${userName}`;
   // const formatteedUserName = (userName) => <span>@{userName}</span>;
+  const [nombre, setNombre] = useState("eli");
   return (
     <section className="App">
       <TwitterFollowCard isFollowing={true}>Test</TwitterFollowCard>
@@ -11,6 +13,14 @@ export function App() {
       <TwitterFollowCard userName="midudev">mouredev</TwitterFollowCard>
 
       <TwitterFollowCard userName="jhonatan">Jhonatan</TwitterFollowCard>
+      <TwitterFollowCard userName={nombre}>Jhonatan</TwitterFollowCard>
+      <button
+        onClick={() => {
+          setNombre("Daniel");
+        }}
+      >
+        Cambiar de Nombre
+      </button>
     </section>
   );
 }
